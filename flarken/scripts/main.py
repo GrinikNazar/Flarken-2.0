d873f736c4e734b4ea645271b4b428defa69287d
GrinikNazar <hrynyknazar@gmail.com>
2026-03-06 00:03:26 +0200

    Changed all parts from excel

--- flarken/scripts/main.py
+++ flarken/scripts/main.py
@@ -13,9 +13,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flarken.settings")
 
 django.setup()
 
-from warehouse.models import Part, PhoneModel, Color, PartType
+from warehouse.models import Part, PhoneModel, Color, PartType, ChipType
 
-sheet_name = 'АКБ'
+sheet_name = 'Екран копія'
 
 def import_excel():
     df = pd.read_excel('data.xlsx', sheet_name=sheet_name)
@@ -23,6 +23,7 @@ def import_excel():
     phone_models = {p.name: p for p in PhoneModel.objects.all()}
     part_types = {p.name: p for p in PartType.objects.all()}
     colors = {c.name: c for c in Color.objects.all()}
+    chip_type = {c.name: c for c in ChipType.objects.all()}
 
     for index, row in df.iterrows():
         phone_model = row['Модель'].strip()
@@ -30,8 +31,15 @@ def import_excel():
         try:
             color = row['Колір'].strip().title()
             color = colors[color]
-        except KeyError:
+        except Exception:
             color = None
+
+        try:
+            chip = row['Тип чіпа']
+            chip_t = chip_type[chip]
+        except Exception:
+            chip_t = None
+
         quantity = row['Наявність']
         min_quantity = row['Мінімум']
         max_quantity = row['Максимум']
@@ -42,18 +50,19 @@ def import_excel():
 
         part_type = part_types[part_type]
 
-
         part = Part(
             part_type = part_type,
             color = color,
             current_quantity = quantity,
             min_quantity = min_quantity,
             max_quantity = max_quantity,
+            chip_type=chip_t,
         )
 
         part.save()
 
         part.phone_models.add(*phone_model)
+
 
 if __name__ == '__main__':
     import_excel()

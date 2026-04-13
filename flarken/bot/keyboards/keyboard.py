2a3ba0b501994489bcb0d14e1400ff4b0384c265
GrinikNazar <hrynyknazar@gmail.com>
2026-04-14 00:05:29 +0300

    key back

--- flarken/bot/keyboards/keyboard.py
+++ flarken/bot/keyboards/keyboard.py
@@ -84,23 +84,29 @@ def check_exists_color_or_chip_type(params):
     return params, ''
 
 
+def add_back_button(markup):
+    markup.row(types.InlineKeyboardButton('⬅️ Назад', callback_data='back'))
+    return markup
+
+
 def show_phone_model_range(part_type):
     markup = types.InlineKeyboardMarkup()
 
     row = [types.InlineKeyboardButton(item.name, callback_data=f'write_off:model_range:{item.pk}') for item in
            PhoneModelRange.objects.filter(phonemodel__supported_part_types=part_type).distinct().order_by('pk')]
-
     markup.add(*row, row_width=4)
+    add_back_button(markup)
 
     return markup
 
-# TODO: зробити кнопку "Назад"
+
 def show_phone_model(part_type, model_range):
     markup = types.InlineKeyboardMarkup()
 
     row = [types.InlineKeyboardButton(model.name, callback_data=f'write_off:model:{model.pk}') for model in
            PhoneModel.objects.filter(phone_model_range=model_range, supported_part_types=part_type).distinct()]
     markup.add(*row)
+    add_back_button(markup)
 
     return markup
 
@@ -113,6 +119,7 @@ def show_color_or_chip_type(part_type, phone_model, **kwargs):
     if colors.exists():
         row = [types.InlineKeyboardButton(c.name, callback_data=f'write_off:color:{c.name}') for c in colors]
         markup.add(*row)
+        add_back_button(markup)
         return markup
 
     chips = ChipType.objects.filter(part__phone_models=phone_model, part__part_type=part_type).distinct()
@@ -120,6 +127,7 @@ def show_color_or_chip_type(part_type, phone_model, **kwargs):
     if chips.exists():
         row = [types.InlineKeyboardButton(c.name, callback_data=f'write_off:chip:{c.name}') for c in chips]
         markup.add(*row)
+        add_back_button(markup)
         return markup
 
     return None
@@ -130,6 +138,7 @@ def show_quantity():
     markup.add(types.InlineKeyboardButton('1', callback_data=f'write_off:quantity:{1}'))
     row = [types.InlineKeyboardButton(f'{i}', callback_data=f'write_off:quantity:{i}') for i in range(2, 10)]
     markup.add(*row)
+    add_back_button(markup)
     return markup
 
 
@@ -137,31 +146,3 @@ def write_off_dep_part(dep_part_type, dep_part_quantity):
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton(f'Списати деталь {dep_part_type}', callback_data=f'write_off:quantity:{dep_part_quantity}'))
     return markup
-
-
-def add_back_button(markup):
-    markup.row(types.InlineKeyboardButton('⬅️ Назад', callback_data='back'))
-    return markup
-
-
-
-# def other_key(user):
-#     users = iphone_db.select_hose()
-#     markup = types.InlineKeyboardMarkup()
-#     work_progress = types.InlineKeyboardButton('WorkProgress',
-#                                                switch_inline_query_current_chat=f'_wp\n{work_progress_db.select_work_progress(user)}')
-#     data_from_bot = types.InlineKeyboardButton('Скинути дані з бота', callback_data='reset-data-from-bot')
-#     null_wp = types.InlineKeyboardButton('Обнулити дані WP', callback_data='reset_data_user')
-#     reset_db_all = types.InlineKeyboardButton('Ресет бази шлангів', callback_data='reset_all_data_user')
-#     markup.row(work_progress)
-#     markup.add(null_wp, data_from_bot)
-#     user_confirm_list = [users['Назар']]  # Список з користувачами адмінами
-#     if user in user_confirm_list:
-#         markup.row(reset_db_all)
-#     return markup
-#
-#
-# def confirm():
-#     markup = types.InlineKeyboardMarkup()
-#     markup.add(types.InlineKeyboardButton('Відправити в чат \U0001F680', callback_data='confirm_button'))
-#     return markup
